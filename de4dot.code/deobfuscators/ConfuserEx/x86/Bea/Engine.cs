cbc27851153937aa9da5be6ea45b254972ddd011
ViR Dash <virb3e@gmail.com>
2017-02-13 11:14:22 +0200

    Implemented ConfuserEx deobfuscator
    x86 cflow and x86 constant decryption
    Backport of LINQ (LinqBridge)
    Shift left/right emulation fixes in de4dot core
    Block class extended to hold additional information

---
+++ de4dot.code/deobfuscators/ConfuserEx/x86/Bea/Engine.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+
+namespace de4dot.Bea
+{
+    public class BeaEngine
+    {
+        [DllImport("BeaEngine")]
+        public static extern int Disasm([In, Out, MarshalAs(UnmanagedType.LPStruct)] Disasm disasm);
+
+        [DllImport("BeaEngine")]
+        private static extern string BeaEngineVersion();
+
+        [DllImport("BeaEngine")]
+        private static extern string BeaEngineRevision();
+
+        public static string Version
+        {
+            get
+            {
+                return BeaEngineVersion();
+            }
+        }
+
+        public static string Revision
+        {
+            get
+            {
+                return BeaEngineRevision();
+            }
+        }
+    }
+}
