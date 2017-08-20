f6d73852e1b519ce5101b061c550c458594d0d07
ViR Dash <virb3e@gmail.com>
2017-08-20 16:25:25 +0300

    Implement ConfuserEx generic constants and resource decryption; misc improvements
    Move BeaEngine.dll to /bin/
    Make sure BeaEngine.dll is loaded when the working directory is different
    Disable file deobfuscation exception handler
    Don't remove LZMA methods by default
    Trim version read from ConfuserAttribute
    Minor refactoring

--- de4dot.code/deobfuscators/ConfuserEx/x86/Bea/Engine.cs
+++ de4dot.code/deobfuscators/ConfuserEx/x86/Bea/Engine.cs
@@ -5,6 +5,18 @@ namespace de4dot.Bea
 {
     public static class BeaEngine
     {
+        // 'de4dot\bin\de4dot.blocks.dll' -> 'de4dot\bin\'
+        private static string _executingPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+        static BeaEngine()
+        {
+            //TODO: Better handle native DLL discovery
+            SetDllDirectory(_executingPath);
+        }
+
+        [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool SetDllDirectory(string lpPathName);
+
         [DllImport("BeaEngine")]
         public static extern int Disasm([In, Out, MarshalAs(UnmanagedType.LPStruct)] Disasm disasm);
 
