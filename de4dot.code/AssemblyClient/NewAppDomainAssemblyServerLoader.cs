56ae0009470103947fbfbb6e40be1cf17bdea4dd
AndyWu <andy.wct@126.com>
2022-05-10 15:55:46 +0800

    update .NET version from v4.5 to v4.8

--- de4dot.code/AssemblyClient/NewAppDomainAssemblyServerLoader.cs
+++ de4dot.code/AssemblyClient/NewAppDomainAssemblyServerLoader.cs
@@ -41,7 +41,7 @@ namespace de4dot.code.AssemblyClient {
 				try {
 #if NET35
 					appDomain.ExecuteAssembly(filename, null, new string[] {
-#elif NET45
+#elif NET48
 					appDomain.ExecuteAssembly(filename, new string[] {
 #else
 #error Unknown tfm
